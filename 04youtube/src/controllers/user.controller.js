a2210a7133f15773d1c142e576309507babd0c5b
Md Selim <whiteblueskyss@gmail.com>
2024-09-01 12:25:36 +0600

    Added register controller

--- 04youtube/src/controllers/user.controller.js
+++ 04youtube/src/controllers/user.controller.js
@@ -1,7 +1,56 @@
+import ApiError from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register user" });
+
+  const { fullname, username, email, password } = req.body;
+
+  if(!fullname.trim()  || !username.trim() || !email.trim() || !password.trim()) {
+    throw new ApiError(400, 'All fields are required');
+  }
+
+  const exitedUser = User.findOne({$or: [{username}, {email}]})
+
+  if(exitedUser) {
+    throw new ApiError(409, 'User already exists');
+  }
+
+  const avatarLocalPath = req.files?.avater[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath || !coverImageLocalPath) {
+    throw new ApiError(400, 'Avatar and cover image are required');
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if(!avatar){
+    throw new ApiError(400, 'Avatar and cover image are required');
+  }
+
+  const user = await User.create({
+    fullname,
+    username,
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage : coverImage.url || ""
+  });
+
+  const createdUser = await User.findById(user._id).select('-password', '-refreshToken');
+
+  if(!createdUser) {
+    throw new ApiError(500, 'User not created');
+  }
+
+  return res.status(201).json(
+    new ApiResponse(201, createdUser, 'User created successfully')
+  );
+
 });
 
 
--- 04youtube/src/routes/user.routes.js
+++ 04youtube/src/routes/user.routes.js
@@ -1,10 +1,18 @@
 import {Router} from 'express';
 import { registerUser } from '../controllers/user.controller.js';
+import { upload } from '../middlewares/multer.middleware.js';
+import ApiError from '../utils/ApiError.js';
 
 const userRouter = Router();
 
 userRouter.route('/register')
-  .post(registerUser);
+  .post(
+    upload.fields([
+      { name: 'avatar', maxCount: 1 },
+      { name: ' coverImage', maxCount: 1 }
+    ]),
+    registerUser  
+  );
 
 
 export default userRouter;
